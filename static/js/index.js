fe7e9ed52080d02bc85f7b23e1ca15537a53c65b
Vivek0712 <vivekraja98@gmail.com>
2020-04-05 14:41:21 +0530

    map update

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,41 @@
+var data = [
+    ['in-py', 0],
+    ['in-ld', 1],
+    ['in-wb', 2],
+    ['in-or', 3],
+    ['in-br', 4],
+    ['in-sk', 5],
+    ['in-ct', 6],
+    ['in-tn', 7],
+    ['in-mp', 8],
+    ['in-2984', 9],
+    ['in-ga', 10],
+    ['in-nl', 11],
+    ['in-mn', 12],
+    ['in-ar', 13],
+    ['in-mz', 14],
+    ['in-tr', 15],
+    ['in-3464', 16],
+    ['in-dl', 17],
+    ['in-hr', 18],
+    ['in-ch', 19],
+    ['in-hp', 20],
+    ['in-jk', 21],
+    ['in-kl', 22],
+    ['in-ka', 23],
+    ['in-dn', 24],
+    ['in-mh', 25],
+    ['in-as', 26],
+    ['in-ap', 27],
+    ['in-ml', 28],
+    ['in-pb', 29],
+    ['in-rj', 30],
+    ['in-up', 31],
+    ['in-ut', 32],
+    ['in-jh', 33]
+];
+
+
 var statecode = {"Andhra Pradesh":"IN-AP",
 "Arunachal Pradesh":	"IN-AR",
 "Assam":	"IN-AS",
@@ -135,17 +173,60 @@ statewiseinfo = {}
     // console.log(arr[0]);
 	// console.log(datastring);
     $("#tablecontent").html(datastring);
+    console.log(statewiseinfo);
+    
+// Prepare demo data
+// Data is joined to map using value of 'hc-key' property by default.
+// See API docs for 'joinBy' for more info on linking data and map.
+var data = [
+    ['in-py', 0],
+    ['in-ld', 1],
+    ['in-wb', 2],
+    ['in-or', 3],
+    ['in-br', 4],
+    ['in-sk', 5],
+    ['in-ct', 6],
+    ['in-tn', 7],
+    ['in-mp', 8],
+    ['in-2984', 9],
+    ['in-ga', 10],
+    ['in-nl', 11],
+    ['in-mn', 12],
+    ['in-ar', 13],
+    ['in-mz', 14],
+    ['in-tr', 15],
+    ['in-3464', 16],
+    ['in-dl', 17],
+    ['in-hr', 18],
+    ['in-ch', 19],
+    ['in-hp', 20],
+    ['in-jk', 21],
+    ['in-kl', 22],
+    ['in-ka', 23],
+    ['in-dn', 24],
+    ['in-mh', 25],
+    ['in-as', 26],
+    ['in-ap', 27],
+    ['in-ml', 28],
+    ['in-pb', 29],
+    ['in-rj', 30],
+    ['in-up', 31],
+    ['in-ut', 32],
+    ['in-jh', 33]
+];
 
-    Highcharts.mapChart('container', {
+// Create the chart
+Highcharts.mapChart('container', {
     chart: {
-        map: 'countries/in/custom/in-all-disputed'
+        map: 'countries/in/in-all'
     },
 
     title: {
         text: 'Active Cases in India'
     },
 
-    
+
+   
     mapNavigation: {
         enabled: true,
         buttonOptions: {
@@ -169,9 +250,7 @@ statewiseinfo = {}
             enabled: true,
             format: '{point.name}'
         }
-    }],
-
-    plotOptions:{
+    }],plotOptions:{
         series:{
             point:{
                 events:{
@@ -191,7 +270,6 @@ statewiseinfo = {}
               }
           }
       },
-
 
 
 
